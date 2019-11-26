22e36d2d5079baec9ee0996bbd4e3fba86de32d1
Thomas Gray <thomas.gray@tgray.local>
2019-11-26 07:57:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+let multiply = num1 * num2
+let random = Math.floor(Math.random()*1000) + 1
+let mod = 20 % 8
+let max = Math.max(1, 4, 5, 9, 20)
