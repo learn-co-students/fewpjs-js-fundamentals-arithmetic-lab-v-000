30a7140ce2a1606cdab9cd69961e607d859a953a
Gingertonic <bethmschofield@gmail.com>
2019-11-28 23:56:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
-// Write your code here
+// Write your codec here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1*num2;
+const random = Math.floor(Math.random()*1000);
+const mod = 16%6
+const max = Math.max(2, 20, 6)
