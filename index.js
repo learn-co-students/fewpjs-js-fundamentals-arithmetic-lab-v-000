8f4b38a18a949144aff9c4a7a4deabca8ba7ec95
NaKia Whitby <whitby.nakia@gmail.com>
2019-12-05 19:56:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
 // Write your code here
+
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1;
+let mod = 16 % 6;
+let max = Math.max(5, 15, 10, 20);
