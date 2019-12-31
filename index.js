cee1a26bb55983b2a7a4607b0a59dfbbd850a6fc
Sushi Chanrai <sushi@pathworks.co>
2019-12-31 13:51:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() + 1);
+
+const mod = 9 % 5
+
+
+const max = Math.max(1, 2, 3, 4, 20);
