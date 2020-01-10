213aeed8a872222593724edee0c8256fbd2d546b
Meg Gutshall <meghan.gutshall@gmail.com>
2020-01-09 23:38:21 -0500

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ let num1 = 2;
 let num2 = 31;
 let multiply = num1 * num2;
 
-let random = Math.ceil(Math.random());
+// let random = Math.ceil(Math.random());
+let random = Math.floor(Math.random() * 10) + 1;
 
 let mod = 4 % 5;
 
