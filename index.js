3f370937d79922d7c058dece7598a81b1fd86511
iamtamtamiam <vutam_a@hotmail.com>
2020-05-09 11:08:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+let num1 = 2
+let num2 = 31
+let multiply = (num1 * num2)
+
+
+// generate a random integer greater than 0.
+let random = Math.random()*10
+
+
+//set to an equation whose remainder is equal to "4".
+let mod = 81 % 7
+
+
+//max that will return "20" as the highest number in the set.
+
+let max = Math.max(1, 2, 3, 7, 20);
