393e884e480ad2fc657c9eac97008022da7e9272
sdcro <sdcrouse91@gmail.com>
2020-05-09 00:09:58 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2; // 62 (Solves Test 1)
+
+// Test 2:
+let random = Number.parseInt(Math.random() * 100 + 777); // Random number > 0, between 777 and 876
+
+// Test 3:
+let mod = (2 ** 5 - 1) % (2 - 5 + 44 / 11 * 3); // Generates a remainder equal to 4 >:)
+
+// Test 4 (variable called max, returns 20 as the highest number in a set):
+let max = Math.max(-Infinity, Math.PI, 5, Math.PI, Math.floor(20.8765), 19.999);
