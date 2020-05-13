790b297b252ada8a420e1d2dae2cb56a33f5ada4
gigi <mmgamao1@gmail.com>
2020-05-12 22:53:17 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+const random = Math.floor( Math.random() * 1000 ) + 1
+const mod = 16 % 6
+const max = Math.max( 5, 10, 15, 20 )
+
+// Create a variable called multiply that will multiply the variables num1 and num2 whose results will equal "62".
+// Create a variable called random that will generate a random integer greater than 0.
+// Create a variable called mod set to an equation whose remainder is equal to "4".
+// Create a variable called max that will return "20" as the highest number in the set.
