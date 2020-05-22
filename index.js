ea5d7311bd7ff830efa6ec1be38f4026a020605f
github username <github email address>
2020-05-22 14:13:06 -0700

    Done.

--- index.js
+++ index.js
@@ -13,10 +13,25 @@
 //   }
 // }
 
+let mod = 4 % 25
+
+
 // function multiply(31, 2) {
-//   31 * 2
+let num1 = 31
+let num2 = 2
+
+const multiply = num1 * num2
+
+// }
+//
+// function multiply(num1, num2) {
+//     return (num1 * num2)
 // }
 
-function multiply(num1, num2) {
-    return (num1 * num2)
-}
+let random = Math.floor(Math.random() * 10)
+// function random() {
+//   return Math.floor(Math.random() * 10)
+// }
+
+
+let max = Math.max(1, 2, 20)
