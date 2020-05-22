454c8e09b79ab7fc0856b227d50034a32036b539
github username <github email address>
2020-05-22 13:50:26 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+
+// const multiply = (num1, num2) => {
+//   num1 * num2
+// }
+
+
+// function multiply(num1, num2) {
+//   if (num1 * num2 == 62) {
+//     return 62
+//   }
+// }
+
+// function multiply(31, 2) {
+//   31 * 2
+// }
+
+function multiply(num1, num2) {
+    return (num1 * num2)
+}
