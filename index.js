2b41063d17d1932ffbb4d22c8c6f2a97d4e6e488
Liz P <parisielizabeth@gmail.com>
2020-07-28 00:03:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1 * num2);
+
+let random = Math.floor(Math.random()) + 1;
+
+let mod = 4 % 5;
+
+let max = Math.max(1, 20)
