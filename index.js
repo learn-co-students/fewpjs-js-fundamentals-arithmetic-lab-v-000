3694071d9f82f36959cf85a4df819aa8a8b09095
Sara Harvey <saraharveynyc@gmail.com>
2020-08-10 18:14:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+let multiply, random, mod, max
+let num1 = 31
+let num2 = 2
+let num3 = 16
+
+multiply = num1 * num2
+random = Math.floor(Math.random() *10) + 1
+mod = num3 % 6
+max = Math.max(2,-3,20,6)
