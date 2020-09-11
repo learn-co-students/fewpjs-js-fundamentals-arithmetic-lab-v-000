11f6af1d85be6a5a9b565be4024129bd62c18464
Eric Johnson <ericjohnson0216@icloud.com>
2020-09-10 21:13:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+var num1 = 31
+var num2 = 2
+var multiply = num1 * num2
 
-// Write your code here
+multiply;
+
+var random = Math.floor(Math.random() * 10) + 1;
+random;
+
+var num3 = 9
+var num4 = 5
+var mod = num3 % num4 
+mod;
+
+var max = Math.max(1, 2, 3, 4, 20);
+max;
