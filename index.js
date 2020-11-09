8107dea78d13e9da4634a5438ca00e598d7efc49
wakenightRyu <bennyhudsonhung@gmail.com>
2020-11-09 01:27:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
-// Write your code here
+let num1 = new Number(2);
+let num2 = new Number(31); 
+
+let multiply = num1 * num2; 
+
+let random = Math.random();
+
+let mod = 4
+
+let max = Math.max(20)
