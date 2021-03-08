aaea8c16ee378d3f53b10a00d5d38e200eb58c4e
github username <d24dust@hotmail.com>
2021-03-08 12:07:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 100) + 1
+
+const mod = 4 % 5
+
+const max = Math.max(...[1,2,3,4,5,6,20])
