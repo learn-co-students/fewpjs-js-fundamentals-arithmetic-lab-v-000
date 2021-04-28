3d4d48151df09c3686d7889a392f01507b224a92
Paul Fell <swellpf@gmail.com>
2021-04-28 01:29:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random());
+
+const num3 = 64;
+const num4 = 15;
+const mod = num3 % num4;
+
+const max = Math.max(2, 17, 5, 20);
