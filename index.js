b612d9b936f41fc6d9ed9481f1b00baa31378da6
capricious-slingshot <ephemere@protonmail.com>
2021-05-08 18:22:37 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+const multiply = num1 * num2
+
+const random = Math.ceil(Math.random());
+
+const mod = 16 % 6
+
+const max = Math.max(16, 17, 18, 19, 20)
+
