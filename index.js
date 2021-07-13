d7da6979913620e7f411a266c6a6b3e912188126
lrh2daizzo <herrera.lrh@gmail.com>
2021-07-13 13:27:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-let num1 = 31 
+let num1 = 31
 let num2 = 2
 
 const multiply = num1 * num2;
-
 const random = Math.ceil(Math.random());
-
 const mod = 16 % 6;
-
 const max = Math.max(20, 5, 7.5, 10);
